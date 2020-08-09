ee0500ad1b21bebb3412c21544649ba4c25e9a3c
Marcos Braga Choma <marcos.choma@tibersoft.com>
2020-08-09 02:41:36 -0300

    Commiting web folder

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+import api from '../../services/api';
+
+export interface Teacher {
+    id: number;
+    name: string;
+    subject: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.avatar} alt="Marcos Braga Choma" />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>
+                {teacher.bio}
+            </p>
+            <footer>
+                <p>Preço/hora <strong>R$ {teacher.cost}</strong></p>
+                <a target="_blank"
+                    rel="noopener norefererr"
+                    href={`https://wa.me/${teacher.whatsapp}`} 
+                    onClick={createNewConnection}>
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </a>
+            </footer>   
+        </article>
+    );
+}
+
+export default TeacherItem
